634c3bde74a4bb5138eef0456e21ee053542c42c
scinscinscin <65155829+scinscinscin@users.noreply.github.com>
2021-10-28 10:01:10 +0800

    create constructor

--- ThrustBody.cs
+++ ThrustBody.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +9,16 @@ namespace SpaceSimulation
     [Serializable]
     public class ThrustBody : TrajectoryBody
     {
-        public List<Double2> thrustKeys;
-		public double rocketLength = 0; // length of the rocket in meters
-		// current_t_data is an inherited value from TrajectoryBody
+        public readonly List<Double2> thrustKeys;
+	public readonly double rocketLength; // length of the rocket in meters
 
-		// override the GetCurrentForces method from TrajectoryBody to add thrust in the forces
+        public ThrustBody(TrajectoryData tdata, double RocketLength, List<Double2> ThrustKeys): base(tdata) {
+            thrustKeys = ThrustKeys;
+            rocketLength = RocketLength;
+        }
+
+        // current_t_data is an inherited value from TrajectoryBody
+	// override the GetCurrentForces method from TrajectoryBody to add thrust in the forces
         protected override Double2 GetCurrentForces(CelestialBody[] otherObjects)
         {
             var newForces = base.GetCurrentForces(otherObjects); // gets gravity of trajectoryBody
@@ -21,8 +26,12 @@ namespace SpaceSimulation
             return newForces;
         }
 
-        // turning energy should be a negative value if the rocket is turning right
-        // in trigonometry clockwise is negative
+        /// <summary>
+        /// Compute the rotational velocity of a rocket initiating a turn
+        /// </summary>
+        /// <param name="turningEnergy"> The amonut of force exerted on the side of a rocket. Negative values represent a right turn while positive values represent a left turn. </param>
+        /// <param name="timespan"> The amount of time that a turn takes place </param>
+        /// <returns></returns>
         double GetRotationalVelocity(double turningEnergy, double timespan)
         {
             // formula for getting the inertia of an object whose pivot is in the center
@@ -33,54 +42,41 @@ namespace SpaceSimulation
 
             // initial momentum should be subtracted from this but the rocket has no angular momentum when the turn is started
             double angularMomentum = finalMomentum / inertia; // this should be in radians / second
-            double result = (angularMomentum / SpaceSimulation.PI) * 180; // convert it to radians / second
+            double result = (angularMomentum / Math.PI) * 180; // convert it to radians / second
             return result;
 		}
 
-        Double2 GetThrust(CelestialBody[] otherObjects, double percentage)
-        {
+        /// <summary>
+        /// Compute for the thrust of the rocket
+        /// </summary>
+        /// <param name="otherObjects">Array of planets, used to determine where the rocket launching from</param>
+        /// <param name="percentage">The thrust level of the rocket</param>
+        /// <returns> The change in position of the rocket with its angle into account</returns>
+        Double2 GetThrust(CelestialBody[] otherObjects, double percentage){
             CelestialBody nearest = null; // the nearest planet
             double nearestDist = 0; // distance of the craft to the nearest planet
 
-			// loop through each planet and try to find the one that is nearest
-			// which is probably where the spacecraft would be launching from
-            foreach (var item in otherObjects)
-            {	
+	    // loop through each planet and try to find the one that is nearest
+	    // which is probably where the spacecraft would be launching from
+            foreach (var item in otherObjects){	
 				// distance of spacecraft to item
-                var newDist = (item.GetPositionAtTime(localSecond) - current_t_data.Pos).magnitude;
+                var newDist = (item.GetPositionAtTime(localSecond) - current_t_data.Pos).Magnitude;
 
-                if (nearest == null)
-                {
-					// set first item as default
+                if (nearest == null){
+		    // set first item as default
                     nearest = item;
                     nearestDist = newDist;
-                }
-                else if (newDist < nearestDist)
-                {	
-					// if newDist is smaller, it is closer, so it becomes new closest planet
+                }else if (newDist < nearestDist){	
+		    // if newDist is smaller, it is closer, so it becomes new closest planet
                     nearest = item;
                     nearestDist = newDist;
                 }
             }
 
-			// get force of the nearest planet to the craft
-			// might be unused since gravity is computed independently of raw thrust
-            double grav = GetRawForce(current_t_data.Pos, nearest);
-
             // get total acceleration of craft.
             double accel = SpaceSimulation.exhaustVelo * (SpaceSimulation.fuelBurnRate) * percentage;
-            Double2 dir = Double2.DirFromAngle(current_t_data.Angle); //going towards ship direction
-            return dir.normalized * accel;
-        }
-
-        double GetRawForce(Double2 pos, CelestialBody body)
-        {
-            // this method returns the raw force of the planet on the body
-            // universal gravity equation = gconst * (m1 * m2 / sqrdist)
-
-            double sqrdist = Math.Pow((pos - body.GetPositionAtTime(localSecond)).magnitude, 2);
-            double rawForce = SpaceSimulation.gconst * (body.current_t_data.mass * current_t_data.mass / sqrdist);
-            return rawForce;
+            Double2 dir = Double2.DirFromAngle(current_t_data.Angle); // take into consideration the current angle of the rocket
+            return dir.Normalized * accel;
         }
     }
 }
