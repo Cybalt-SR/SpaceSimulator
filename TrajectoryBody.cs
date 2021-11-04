96d7fa16ddcea0508e96293f27168dfbd5ef4a25
Cybalt-SR <cybalt.sr@gmail.com>
2021-11-04 10:48:41 +0800

    new file for body

--- TrajectoryBody.cs
+++ TrajectoryBody.cs
@@ -6,47 +6,6 @@ using UnityEngine;
 
 namespace SpaceSimulation
 {
-    [Serializable]
-
-    public struct TrajectoryData
-    {
-
-#if UNITY_EDITOR
-        [SerializeField]
-#endif
-        private double mass;
-        public double GetMass() => mass;
-
-        public Double2 Pos;
-        public Double2 Velocity;
-        public Double2 Force;
-
-        public double Angle;
-        public double AngularVelocity;
-        public double Torque;
-
-        /// <summary>
-        /// Represents a snapshot of a body's trajectory, such as it's position, velocity
-        /// </summary>
-        /// <param name="Mass"> Mass of the object </param>
-        /// <param name="position"> Position of the object </param>
-        /// <param name="velocity"> Velocity of the object </param>
-        /// <param name="angle"> Angle of the object </param>
-        /// <param name="angularVelocity"> Angular velocity of the object </param>
-        public TrajectoryData(double Mass, Double2 position, Double2 velocity, double angle, double angularVelocity)
-        {
-
-            mass = Mass;
-
-            Pos = position;
-            Velocity = velocity;
-            Force = Double2.Zero;
-
-            Angle = angle;
-            AngularVelocity = angularVelocity;
-            Torque = 0;
-        }
-    }
 
     public abstract class TrajectoryBody
     {
