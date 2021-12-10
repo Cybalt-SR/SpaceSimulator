4f72295f893129d8be1989e1e15c2cbe233c32ef
scinorandex <scinorandex1@gmail.com>
2021-12-10 13:43:48 +0800

    fixes unstatic properties

--- SpaceSimulation.cs
+++ SpaceSimulation.cs
@@ -5,10 +5,10 @@ namespace SpaceSimulation
     // "universal constants"
     public static class SpaceSimulation
     {
-        public const double gconst = 0.000000000066743;
+        public static double gconst = 0.000000000066743;
         public static double EarthMass = 5.9722 * Math.Pow(10, 24); // in kg
-        public const double EarthRadius = 6371000;	// in meters
-        public const double SaturnVRocketLength = 111; // in meters
+        public static double EarthRadius = 6371000;	// in meters
+        public static double SaturnVRocketLength = 111; // in meters
         public const double SaturnVMass = 2812272.69; // in kg
     }
 }
